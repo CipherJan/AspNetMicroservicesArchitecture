b332c9daa9d4b2bd78b90d8a2cfc63e72025b80b
CipherJan <85920527+CipherJan@users.noreply.github.com>
2022-02-21 13:00:47 +0700

    Fill Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,82 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _concext;
+        public ProductRepository(ICatalogContext context)
+        {
+            _concext = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _concext
+                            .Products
+                            .Find(p => true)
+                            .ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _concext
+                            .Products
+                            .Find(p => p.Id == id)
+                            .FirstOrDefaultAsync();
+
+
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+            return await _concext
+                            .Products
+                            .Find(filter)
+                            .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategory(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+            return await _concext
+                            .Products
+                            .Find(filter)
+                            .ToListAsync();
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _concext.Products.InsertOneAsync(product);
+        }
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _concext
+                                        .Products
+                                        .ReplaceOneAsync(filter: f => f.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged && updateResult.MatchedCount > 0;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            var deleteResult = await _concext
+                                        .Products
+                                        .DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+
+    }
+}
